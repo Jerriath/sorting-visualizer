d159c937a71bbd15209c0147e15e819dd53b0377
Jerry Zhang <jerryzhang101@yahoo.com>
2022-07-14 14:08:30 -0400

    Added some components and states

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+// Importing node modules
+import * as react from 'react';
+
+//Importing styles
 import './App.css';
 
+// Destructuring react functionalities
+const {useState, useEffect} = react;
+
+
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <Visualizer />
+      <Footer />
     </div>
   );
 }
