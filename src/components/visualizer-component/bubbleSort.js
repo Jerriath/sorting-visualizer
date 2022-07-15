c3966215390113d1b29aa186e9f259f1194e5bb4
Jerry Zhang <jerryzhang101@yahoo.com>
2022-07-15 13:43:36 -0400

    Refactored bubble sort to iterate through recursively with settimeouts

--- src/components/visualizer-component/bubbleSort.js
+++ src/components/visualizer-component/bubbleSort.js
@@ -2,22 +2,35 @@ import {v4 as uuid} from 'uuid';
 import Element from '../element-component/Element';
 
 const bubbleSort = (array, stopSort, setElementsArray) => {
-  for (let i = 0; i < array.length - 1; i++) {
-    setTimeout(() => {
-      let temp = [...array];
-      let heightFirst = array[i].props.height;
-      let heightSecond = array[i+1].props.height
-      temp[i] = <Element key={uuid()} height={heightFirst} classList='element sorting' />;
-      temp[i + 1] = <Element key={uuid()} height={heightSecond} classList='element sorting' />;
-      setElementsArray(temp);
-    }, 100 * i)
-  }
+  let temp = [...array];
+  recursiveCheck(temp, 0, setElementsArray);
   setTimeout(() => {
     stopSort();
     let temp = [...array];
     setElementsArray(temp);
-  }, 100 * array.length)
+  }, 500 * array.length)
   return;
+}
+
+const recursiveCheck = (temp, i, setElementsArray) => {
+  setTimeout(() => {
+    console.log(i); 
+    let heightFirst = temp[i].props.height;
+    let heightSecond = temp[i+1].props.height
+    temp = temp.map((el, i) => <Element key={uuid()} height={temp[i].props.height} classList='element' />)
+    temp[i] = <Element key={uuid()} height={heightFirst} classList='element checking' />;
+    temp[i + 1] = <Element key={uuid()} height={heightSecond} classList='element checking' />;
+    if (heightSecond > heightFirst) {
+      // switching(array, i, setElementsArray)
+    }
+    setElementsArray(temp);
+    if (i < temp.length - 2) {
+      recursiveCheck(temp, i+1, setElementsArray);
+    }
+    else {
+      return;
+    }
+  }, 500 )
 }
 
 export default bubbleSort;
