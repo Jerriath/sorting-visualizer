67d330051169d264ec9ce29cb99ec2ba87ab79c0
Jerry Zhang <jerryzhang101@yahoo.com>
2022-07-18 15:54:12 -0400

    Added finished first version of insertionSort; currently doesn't work; rewrites height values

--- src/components/visualizer-component/sorting-functions/insertionSort.js
+++ src/components/visualizer-component/sorting-functions/insertionSort.js
@@ -8,18 +8,21 @@ const insertionSort = (array, stopSort, setElementsArray, speed) => {
   return;
 }
 
+// Function to check the next value in the array to be sorted
 const incrementCheck = (temp, i, stopSort, setElementsArray, speed) => {
   setTimeout(() => {
     if (i < temp.length) {
       temp = [...temp];
       let currentHeight = temp[i].props.height;
       let checkingHeight = temp[i-1].props.height;
-      temp = temp.map((el, index) => <Element key={uuid()} height={temp[index].props.height} classList='element' />)
+      temp = temp.map((el, index) => <Element key={uuid()} height={temp[index].props.height} classList='element' />);
       temp[i] = <Element key={uuid()} height={currentHeight} classList='element checking' />;
       temp[i-1] = <Element key={uuid()} height={checkingHeight} classList='element checking' />;
       setElementsArray(temp);
       if (currentHeight < checkingHeight) {
-        // This conditional happens if the two elements need to be swapped
+        setTimeout(() => {
+          swap(temp, i, i, stopSort, setElementsArray, speed);
+        }, speed)
       }
       // Eventuallly will be an else if
       if (i < temp.length) {
@@ -34,20 +37,42 @@ const incrementCheck = (temp, i, stopSort, setElementsArray, speed) => {
   }, speed)
 }
 
-const decrementCheck = () => {
-
+// Function initiates the swapping process between the workingIndex and previous by switching their color
+// Stores currentIndex so that when the workingIndex is sorted, incrementCheck and work on currentIndex + 1
+const swap = (temp, currentIndex, workingIndex, stopSort, setElementsArray, speed) => {
+  temp = [...temp];
+  temp[workingIndex] = <Element key={uuid()} height={temp[workingIndex].props.height} classList='element sorting' />;
+  temp[workingIndex-1] = <Element key={uuid()} height={temp[workingIndex].props.height} classList='element sorting' />;
+  setElementsArray(temp);
+  setTimeout(() => {
+    swapping(temp, currentIndex, workingIndex, stopSort, setElementsArray, speed);
+  }, speed)
 }
 
-const swapping = () => {
-
+// Function takes care of actually swapping the workingIndex and previous
+const swapping = (temp, currentIndex, workingIndex, stopSort, setElementsArray, speed) => {
+  temp = [...temp];
+  let currentElement = temp[workingIndex];
+  temp[workingIndex] = temp[workingIndex-1];
+  temp[workingIndex-1] = currentElement;
+  setElementsArray(temp);
+  setTimeout(() => {
+    decrementCheck(temp, currentIndex, workingIndex, stopSort, setElementsArray, speed);
+  }, speed)
 }
 
-const swap = () => {
-
-}
-
-const next = () => {
-
+// Checks to see if there needs to be another swap; if so repeat swap with workingIndex
+const decrementCheck = (temp, currentIndex, workingIndex, stopSort, setElementsArray, speed) => {
+  temp = temp = temp.map((el, index) => <Element key={uuid()} height={temp[index].props.height} classList='element' />);
+  setElementsArray(temp);
+  if (temp[workingIndex - 1] > temp[workingIndex - 2]) {
+    setTimeout(() => {
+      swap(temp, currentIndex, workingIndex - 1, stopSort, setElementsArray, speed);
+    }, speed)
+  }
+  else {
+    incrementCheck(temp, currentIndex + 1, stopSort, setElementsArray, speed);
+  }
 }
 
 export default insertionSort;
